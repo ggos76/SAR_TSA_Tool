82bcf29303f823e3279c762b8086276a4e87b36a
ggos76 <gabriel.gosselin@hotmail.ca>
2025-05-22 14:31:13 -0400

    First commit of a version control function that verify if the minimum version of Catalyst and Python are meet.

---
+++ TSA_utilities/SAR_TSA_version_control.py
@@ -0,0 +1,31 @@
+
+'''----------------------------------------------------------------------------------------------
+ * Gabriel Gosselin, CCRS   2024-2025                                                           -
+ * -----------------------------------------------------------------------------------------------
+'''
+
+import sys
+
+def version_control (vs_catalyst, vs_python):
+    
+    min_python_vs = (3,10,0) 
+    print ("Minimum Python version to run the TSA scripts: " + str(min_python_vs))
+    print ("Current Python version: " + str(vs_python))
+    if min_python_vs < vs_python: 
+        print ("Python version requirement is meet")
+    else: 
+        print ("Error - The minimum python version requirement is not meet")
+        sys.exit()
+     
+    min_catalyst_vs =(3,1,50)
+    version_tuple = tuple(map(int, vs_catalyst.split(".")))
+    vs_catalyst = version_tuple
+    print ("Minimum Catalyst install to run the TSA scripts: " + str(min_catalyst_vs))
+    print ("Current Catalyst install: " + str(vs_catalyst))
+    if min_catalyst_vs < vs_catalyst:
+        print ("Catalyst version requirement is meet")
+    else:
+        print ("Error - The minimum Catalyst version requirement is not meet")
+        sys.exit()
+
+    return ()
